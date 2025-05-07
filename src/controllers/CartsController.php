df81b71e0b31f55adf6cc3cedf72b5fb295389ee
Josh Crawford <joshua@sgroup.com.au>
2025-05-08 08:00:42 +1000

    Fix an error when trying to recover an expired cart

--- src/controllers/CartsController.php
+++ src/controllers/CartsController.php
@@ -45,6 +45,8 @@ class CartsController extends Controller
 
     public function actionFindCarts(): Response
     {
+        $session = Craft::$app->getSession();
+
         $requestPasskey = $this->request->getParam('passkey');
         $passKey = AbandonedCart::$plugin->getSettings()->getPassKey();
 
@@ -52,7 +54,7 @@ class CartsController extends Controller
             $abandonedCarts = AbandonedCart::$plugin->getCarts()->getEmailsToSend();
 
             if ($abandonedCarts) {
-                Craft::$app->getSession()->setNotice(Craft::t('abandoned-cart', '{num} abandoned carts were queued.', ['num' => $abandonedCarts]));
+                $session->setNotice(Craft::t('abandoned-cart', '{num} abandoned carts were queued.', ['num' => $abandonedCarts]));
             }
             
             return Craft::$app->controller->redirect(UrlHelper::cpUrl('abandoned-cart'));
@@ -63,6 +65,8 @@ class CartsController extends Controller
 
     public function actionRestoreCart()
     {
+        $session = Craft::$app->getSession();
+
         $number = $this->request->getParam('number');
         $order = Order::find()->number($number)->one();
 
