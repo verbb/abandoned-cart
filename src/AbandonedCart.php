614280938b2b2164f072b6bbd4ff64e83993a23b
Josh Crawford <joshua@sgroup.com.au>
2025-05-16 13:17:36 +1000

    Up schemaVersion

--- src/AbandonedCart.php
+++ src/AbandonedCart.php
@@ -23,7 +23,7 @@ class AbandonedCart extends Plugin
 
     public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
-    public string $schemaVersion = '1.0.1';
+    public string $schemaVersion = '2.0.1';
 
 
     // Traits
